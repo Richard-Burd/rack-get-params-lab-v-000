791b3378be999a6d8c4a7facdf83766377f90e5a
Richard Burd <richardburdx@yahoo.com>
2017-10-25 19:49:27 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,6 +16,9 @@ class Application
       resp.write handle_search(search_term)
 
    elsif req.path.match(/cart/)
+     if @@cart.empty?
+       resp.write "Your cart is empty"
+     else 
      @@cart.each do |thingy|
        resp.write "#{thingy}\n"
      end
